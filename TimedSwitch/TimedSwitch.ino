e5e1fed8816df61f7f80482fdec126bedad82dc1
Dimitris Paxinos <dpaxinos@gmail.com>
2017-03-05 23:48:37 +0200

    timed switch first attempt

--- TimedSwitch/TimedSwitch.ino
+++ TimedSwitch/TimedSwitch.ino
@@ -36,7 +36,7 @@ const int buttonPin = 10;    // the number of the pushbutton pin
 const int ledPin = 5;      // the number of the LED pin
 
 						   // Variables will change:
-int ledState = HIGH;         // the current state of the output pin
+int ledState = LOW;         // the current state of the output pin
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 
@@ -44,6 +44,8 @@ int lastButtonState = LOW;   // the previous reading from the input pin
 							 // will quickly become a bigger number than can be stored in an int.
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 120;    // the debounce time; increase if the output flickers
+unsigned long nextUpdateTime = 0;
+
 
 void setup() {
 	pinMode(buttonPin, INPUT);
@@ -57,13 +59,33 @@ int reading = 0;
 void loop() {
 	// read the state of the switch into a local variable:
 	int reading = digitalRead(buttonPin);
-	debounce(buttonPin, ledPin, reading, buttonState, lastButtonState, lastDebounceTime, debounceDelay, ledState);
+	//Serial.println(reading);
+
+	if (reading == 1 && nextUpdateTime == 0)
+	{
+		nextUpdateTime = millis() + 15000;
+		Serial.println(nextUpdateTime);
+		ledState = debounce(buttonPin, ledPin, reading, buttonState, lastButtonState, lastDebounceTime, debounceDelay, ledState);
+		digitalWrite(ledPin, ledState);
+	}
+	if (nextUpdateTime>0 && nextUpdateTime < millis())
+	{
+		nextUpdateTime = 0;
+		Serial.println("Back to zero");
+		Serial.println(millis());
+		reading = LOW;
+		ledState = LOW;
+		digitalWrite(ledPin, ledState);
+	}
+
+
 	lastButtonState = reading;
 }
 
 
-void debounce(int btnpin, int lpin, int reading, int &btnState, int lastbtnState, unsigned long &lDTime, unsigned long delay, int &lState)
-{	
+int debounce(int btnpin, int lpin, int reading, int &btnState, int lastbtnState, unsigned long &lDTime, unsigned long delay, int lState)
+{
+
 	// check to see if you just pressed the button
 	// (i.e. the input went from LOW to HIGH),  and you've waited
 	// long enough since the last press to ignore any noise:
@@ -86,7 +108,7 @@ void debounce(int btnpin, int lpin, int reading, int &btnState, int lastbtnState
 			if (btnState == HIGH) {
 				lState = !lState;
 			}
+			return lState;
 		}
-		digitalWrite(lpin, lState);
 	}
 }
